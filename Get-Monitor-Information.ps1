2ab85ae3b556595c5487eb90d86d40fadd0910f2
Josh Dwight <joshdwight101@gmail.com>
2024-09-29 17:40:01 -0500

    Get-Monitor-Information.ps1
    Get-Monitor-Information.ps1
    
    This script is a simple script that attempts to obtain information about the display monitors that are hooked up to a workstation. By default it outputs to C:\Temp, but the variables are at the top of the script for easy editing.

---
+++ Get-Monitor-Information.ps1
@@ -0,0 +1,18 @@
+﻿# Get-Monitor-Information.ps1
+# Author: Josh Dwight
+# https://github.com/joshdwight101/PowerShell
+
+$output_path = "C:\Temp"  # path to drop the log into when script is ran
+$output_file = "$output_path\$env:computername-monitor-info.log" 
+
+
+function Get-Monitor-Info {
+    Get-CimInstance WmiMonitorID -Namespace root\wmi | ForEach-Object {
+    $Serial = [System.Text.Encoding]::ASCII.GetString($_.SerialNumberID).Trim(0x00)
+    Write-Output "Monitor Serial Number: $Serial"
+    }
+}
+
+
+Get-Monitor-Info | Out-File -FilePath $output_file
+& Notepad $output_file
